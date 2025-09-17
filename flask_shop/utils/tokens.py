256166c4f5906d476e881c4981e8f2de673068c3
unibody <guopp1028@gmail.com>
2025-09-17 11:36:47 +0800

    utils/tokens下增加了登录装饰器login_required

--- flask_shop/utils/tokens.py
+++ flask_shop/utils/tokens.py
@@ -5,9 +5,12 @@
 '''
 
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app,request
 from flask_shop.models import User
 from itsdangerous import BadSignature,SignatureExpired  # 导入异常类
+from flask_shop.utils.message import to_dict_msg
+import functools
+
 
 def generate_auth_token(uid,expiration):
     # 创建加密对象
@@ -33,3 +36,20 @@ def verify_auth_token(token_str) -> User | None:
     # 获取用户实例
     usr = User.query.filter_by(id=data['id']).first()
     return usr
+
+def login_required(view_func):
+    @functools.wraps(view_func) #用@装饰器保留原函数元信息
+    def verify_token(*args,**kwargs):
+        try:
+            token = request.headers['token']
+        except Exception:
+            return to_dict_msg(10003)
+
+        user = verify_auth_token(token)
+        if not user:
+            # 根据不同错误类型返回错误码
+            return to_dict_msg(10004, msg="token无效或已过期")
+
+        return view_func(*args,**kwargs)
+
+    return verify_token
