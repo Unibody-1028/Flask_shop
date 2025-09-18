80a303e1219aebd6bf586b6c7cba858fc145835c
unibody <guopp1028@gmail.com>
2025-09-18 11:29:55 +0800

    后端创建Menu类用以创建菜单+修复数据库迁移错误

--- flask_shop/models.py
+++ flask_shop/models.py
@@ -46,3 +46,29 @@ class User(db.Model,BaseModel):
         '''
         return check_password_hash(self.pwd,t_pwd)
 
+class Menu(db.Model):
+    __tablename__ = 't_menu'
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    name = db.Column(db.String(32),unique=True,nullable=False)
+    level = db.Column(db.Integer,nullable=False)
+    path = db.Column(db.String(32))
+
+    pid = db.Column(db.Integer)
+    children = db.relationship('Menu')
+
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+            'level':self.level,
+            'path':self.path,
+            'pid':self.pid,
+            'children':self.get_child_list()
+        }
+    def get_child_list(self):
+        obj_child = self.children
+        data = []
+        for o in obj_child:
+            data.append(o.to_dict())
+
+        return data
