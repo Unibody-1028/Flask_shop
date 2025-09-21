4bc36643ad30c0c2cd7e351a7cbf52e54ce0fa73
unibody <guopp1028@gmail.com>
2025-09-21 21:23:22 +0800

    修正sql错误数据

--- data.sql
+++ data.sql
@@ -8,7 +8,7 @@ insert into t_menu (id,name,level,pid,path) value(21,'用户列表',2,2,'/user_l
 insert into t_menu (id,name,level,pid,path) value(31,'角色列表',2,3,'/auther_list');
 insert into t_menu (id,name,level,pid,path) value(32,'权限列表',2,3,'/role_list');
 insert into t_menu (id,name,level,pid,path) value(41,'商品列表',2,4,'/product_list');
-insert into t_menu (id,name,level,pid,path) value(43,'分类列表',2,4,'/group_list');
+insert into t_menu (id,name,level,pid,path) value(42,'分类列表',2,4,'/group_list');
 
 
 INSERT INTO t_user (name, pwd, nick_name, phone, email) VALUES
