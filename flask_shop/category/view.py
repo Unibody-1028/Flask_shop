639ad1d28e47f64590abf9ce194271950f70e099
unibody <guopp1028@gmail.com>
2025-09-22 23:31:27 +0800

    添加商品分类的删除、修改、查找接口

--- flask_shop/category/view.py
+++ flask_shop/category/view.py
@@ -1,3 +1,5 @@
+from tkinter.font import names
+
 from flask import request
 from flask_shop.category import category,category_api    # 导入蓝图,Api实例
 from flask_shop import models,db
@@ -21,8 +23,59 @@ class Category(Resource):
                 return to_dict_msg(200,msg='增加商品类别成功')
             return to_dict_msg(10002)
         except Exception as e:
-
             return to_dict_msg(20000)
+
+    def get(self):
+        try:
+            cid = request.args.get('cid')
+            c = models.Category.query.get(cid)
+            if c:
+                return to_dict_msg(200,c.to_dict(),'获取商品分类信息成功')
+            else:
+                return to_dict_msg(status=10019)
+
+        except Exception as e:
+            return to_dict_msg(20000)
+
+    def put(self):
+        """
+        处理PUT请求,用于更新商品分类信息
+        接收分类ID和新名称,验证后更新数据库中对应的名称
+        :return: JSON响应信息
+        """
+        try:
+            # 从表单中获取分类ID和新名称
+            cid = request.form.get('cid')
+            name = request.form.get('name')
+            # 根据id查询对应的数据
+            c = models.Category.query.get(cid)
+            # 判断分类是否存在
+            if c:
+                # 如果提供了新的名称,则更新分类名称,否则保持原名称不变
+                if name is not None:
+                    c.name = name
+                # 提交事务,将修改保存到数据库
+                db.session.commit()
+                return to_dict_msg(status=200,msg='更新商品分类信息成功!!!')
+            # 若分类不存在,返回错误信息:数据不存在
+            return to_dict_msg(status=10019)
+        except Exception as e:
+            return to_dict_msg(20000)
+
+    def delete(self):
+        try:
+            cid = request.form.get('cid')
+            if cid is not None:
+                c = models.Category.query.get(cid)
+            else:
+                return to_dict_msg(10002)
+            if c:
+                db.session.delete(c)
+                db.session.commit()
+                return to_dict_msg(200,msg='删除商品分类成功')
+        except Exception as e:
+            return to_dict_msg(20000)
+
 
 category_api.add_resource(Category,'/category')
 
